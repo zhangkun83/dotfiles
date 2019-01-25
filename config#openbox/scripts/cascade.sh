fd73d839b53db43d53a87d5cc4f6ad6caf724047
Kun Zhang <zhangkun@google.com>
2019-01-25 13:01:32 -0800

    cascade.sh: use windowactivate instead of windowraise to make sure moved windows are on top of the stack

--- config#openbox/scripts/cascade.sh
+++ config#openbox/scripts/cascade.sh
@@ -5,6 +5,6 @@ y=0
 for windowId in `xdotool search --onlyvisible --classname "$CLASSNAME"`; do
     # Un-maximize
     wmctrl -ir $windowId -b remove,maximized_vert,maximized_horz
-    xdotool windowmove $windowId x $y windowraise $windowId
+    xdotool windowmove $windowId x $y windowactivate $windowId
     ((y+=$STEP))
 done
