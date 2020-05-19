e51e72c83e8dda313375d5a4526cf45533c1cbf5
Kun Zhang <zhangkun@google.com>
2020-05-19 10:57:04 -0700

    openbox: lock.sh run hci-adjustments *after* unlock

--- config#openbox/scripts/lock.sh
+++ config#openbox/scripts/lock.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-XSECURELOCK_FONT=--courier-bold-r-normal--34------- XSECURELOCK_WANT_FIRST_KEYPRESS=1 xsecurelock &
-$DIR/hci-adjustments
+(XSECURELOCK_FONT=--courier-bold-r-normal--34------- XSECURELOCK_WANT_FIRST_KEYPRESS=1 xsecurelock; $DIR/hci-adjustments) &
