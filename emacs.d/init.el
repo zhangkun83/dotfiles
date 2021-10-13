2e9bc4a98571038257cdbfe93d44329a93646e7e
Kun Zhang <zhangkun@google.com>
2021-10-13 13:07:31 -0700

    Use light color theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -197,4 +197,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(region ((t (:background "blue" :foreground "white")))))
