526d8f97dd7d89577678c11e6d8656973e1255af
Kun Zhang <arthur.kun@gmail.com>
2022-03-23 11:52:32 -0700

    xfce4-genmon-plugin-weather.sh: handle special temp format

--- config#openbox/scripts/xfce4-genmon-plugin-weather.sh
+++ config#openbox/scripts/xfce4-genmon-plugin-weather.sh
@@ -3,6 +3,9 @@
 ZIPCODE="$1"
 LABEL="$2"
 FULLRESULT=$(curl wttr.in/$ZIPCODE?1ATm)
-TEMP=$(echo "$FULLRESULT" | grep -o '[0-9]* °C' | head -1 | tr -d ' ')
-echo "<txt><span size='x-small'>$LABEL</span>$TEMP</txt>"
+
+# TEMP format is either "19 °C" or "+18(19) °C"
+TEMP=$(echo "$FULLRESULT" | grep -o '+\?[0-9]*\(([0-9]*)\)\? °C' | grep -o '[0-9]*' | head -1)
+
+echo "<txt><span size='x-small'>$LABEL </span>$TEMP°</txt>"
 echo -e "<tool><span font='mono' size='x-small'>Updated on $(date)\n$FULLRESULT</span></tool>"
