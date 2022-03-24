16d82d6f54d28df2150e3223625707e0f884f26f
Kun Zhang <arthur.kun@gmail.com>
2022-03-23 22:10:51 -0700

    xfce4-genmon-plugin-weather.sh: encode location string; shorten timestamp

--- config#openbox/scripts/xfce4-genmon-plugin-weather.sh
+++ config#openbox/scripts/xfce4-genmon-plugin-weather.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # To be used with xfce4-genmon-plugin
-ZIPCODE="$1"
-LABEL="$2"
-FULLRESULT=$(curl wttr.in/$ZIPCODE?1ATm)
+location="$1"
+label="$2"
+encoded_location=$(python3 -c "import urllib.parse; print(urllib.parse.quote(input()))" <<< "$location")
+
+fullresult=$(curl wttr.in/$encoded_location?1ATm)
 
 # TEMP format is either "19 °C" or "+18(19) °C"
-TEMP=$(echo "$FULLRESULT" | grep -o '+\?[0-9]*\(([0-9]*)\)\? °C' | grep -o '[0-9]*' | head -1)
+TEMP=$(echo "$fullresult" | grep -o '+\?[0-9]*\(([0-9]*)\)\? °C' | grep -o '[0-9]*' | head -1)
 
-echo "<txt><span size='x-small'>$LABEL </span>$TEMP°</txt>"
-echo -e "<tool><span font='mono' size='x-small'>Updated on $(date)\n$FULLRESULT</span></tool>"
+echo "<txt><span size='x-small'>$label </span>$TEMP°</txt>"
+echo -e "<tool><span font='mono' size='x-small'>Updated at $(date +%H:%M)\n$fullresult</span></tool>"
