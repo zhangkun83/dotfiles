a5519e89e797a82c7d0583cbf0e1daaae5f95ba1
Kun Zhang <arthur.kun@gmail.com>
2022-03-27 15:35:27 -0700

    xfce4-genmon-plugin-afk.sh: do not use mono font for applet display

--- config#openbox/scripts/xfce4-genmon-plugin-afk.sh
+++ config#openbox/scripts/xfce4-genmon-plugin-afk.sh
@@ -2,5 +2,5 @@
 # To be used with xfce4-genmon-plugin
 AFKRESULT=$(~/.config/openbox/scripts/afk-report ~/.afk.log today)
 DAILY_HOURS=$(echo "$AFKRESULT" | tail -1 | awk '{print $2}')
-echo -e "<txt><span size='x-small'>HRS</span>\n<span font='mono'>$DAILY_HOURS</span></txt>"
+echo -e "<txt><span size='x-small'>HRS</span>\n$DAILY_HOURS</txt>"
 echo "<tool><span font='mono' size='x-small'>$AFKRESULT</span></tool>"
