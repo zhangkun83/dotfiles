c6288c79206bd9ab0eb10c0894fa102e239b6143
Kun Zhang <zhangkun@google.com>
2022-04-07 10:59:06 -0700

    xfce4-genmon-plugin-weather.sh: do not use current weather condition from the website as it's always outdated

--- config#openbox/scripts/xfce4-genmon-plugin-weather.sh
+++ config#openbox/scripts/xfce4-genmon-plugin-weather.sh
@@ -4,10 +4,33 @@ location="$1"
 label="$2"
 encoded_location=$(python3 -c "import urllib.parse; print(urllib.parse.quote(input()))" <<< "$location")
 
-fullresult=$(curl wttr.in/$encoded_location?1ATm)
+# Current weather condition (first 8 lines) is almost always outdated.
+# Skip them and only use forecasts
+fullresult=$(curl wttr.in/$encoded_location?1ATm | tail +8)
 
+# Capture the forecast temps. 4 values to be filled in the array.
 # TEMP format is either "19 °C" or "+18(19) °C"
-TEMP=$(echo "$fullresult" | grep -o '+\?[0-9]*\(([0-9]*)\)\? °C' | grep -o '[0-9]*' | head -1)
+# Remove the number in the parenthesis (I don't know what it means).
+temps=($(echo "$fullresult" | grep -o '+\?[0-9]*\(([0-9]*)\)\? °C' | sed 's/([0-9]*)//g' | grep -o '[0-9]*'))
 
-echo "<txt><span size='x-small'>$label </span>$TEMP°</txt>"
+# Widget displays forecasted weather based on current time
+current_hour=$(date +%H)
+if [[ $current_hour -lt 7 ]]; then
+    temp=${temps[3]}
+    time_label="夜"
+elif [[ $current_hour -lt 12 ]]; then
+    temp=${temps[0]}
+    time_label="朝"
+elif [[ $current_hour -lt 17 ]]; then
+    temp=${temps[1]}
+    time_label="午"
+elif [[ $current_hour -lt 22 ]]; then
+    temp=${temps[2]}
+    time_label="夕"
+else
+    temp=${temps[3]}
+    time_label="夜"
+fi
+
+echo -e "<txt><span size='x-small'>$label\n$time_label </span>$temp°</txt>"
 echo -e "<tool><span font='Liberation Mono' size='x-small'>Updated at $(date +%H:%M)\n$fullresult</span></tool>"
