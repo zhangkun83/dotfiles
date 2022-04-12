7600360f9a848977d7febf7a63c2c6c6eebce062
Kun Zhang <arthur.kun@gmail.com>
2022-04-12 16:58:38 -0700

    openbox: assign an icon to shutdown-xterm.sh

--- config#openbox/scripts/shutdown-xterm.sh
+++ config#openbox/scripts/shutdown-xterm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 xterm -geometry 40x10+1000+1000 \
       -T 'Shutdown' \
       -bc -fa 'Terminus' -fs 12 \
+      -xrm "xterm*iconHint: $DIR/system-shutdown.xpm" \
       -e bash -c "echo Enter password to shutdown the computer; sudo poweroff"
