d0297c930a1392f0f33010c0d006af5950487c56
Kun Zhang <arthur.kun@gmail.com>
2022-05-04 10:10:41 -0700

    emacs: make zk-transform-patch.py recognize git diff output

--- emacs.d/bin/zk-transform-patch.py
+++ emacs.d/bin/zk-transform-patch.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python3
-# Transforms a patch (standard diff output) into a format that can be
-# recognized by zk's custom compile error pattern
+# Annotates a patch (standard diff output or git diff) so that it can be
+# annotated by the ZK Diff Navigation mode
 
 import os
 import re
 import sys
 
 ZK_PROJECT_ROOT = os.getenv('ZK_PROJECT_ROOT')
-FILENAME_LINE_PATTERN = re.compile(u'^==== \S+ - (\S+) ====$')
-HUNK_HEADER_PATTERN = re.compile(u'^[0-9,]+[cad]([0-9]+)(,[0-9]+)?$')
+FILENAME_LINE_PATTERNS = {
+    "diff": re.compile(u'^==== \S+ - (\S+) ====$'),
+    "git": re.compile(u'^\+\+\+ b/(.*)$'),
+}
+
+HUNK_HEADER_PATTERNS = {
+    "diff": re.compile(u'^[0-9,]+[cad]([0-9]+)(,[0-9]+)?$'),
+    "git": re.compile(u'^@@ -[0-9,]+ \+([0-9]+),[0-9]+ @@'),
+}
+
+format = None
 current_file = None
 for line in sys.stdin:
-    filename_m = re.match(FILENAME_LINE_PATTERN, line)
+    filename_m = None
+    if not format:
+        for f,p in FILENAME_LINE_PATTERNS.items():
+            filename_m = re.match(p, line)
+            if filename_m:
+                format = f
+                break
+    else:
+        filename_m = re.match(FILENAME_LINE_PATTERNS[format], line)
     if filename_m:
         current_file = filename_m.group(1)
         if current_file.startswith(ZK_PROJECT_ROOT):
             current_file = current_file[len(ZK_PROJECT_ROOT):]
-    hunk_m = re.match(HUNK_HEADER_PATTERN, line)
-    if hunk_m:
-        line_number = hunk_m.group(1)
-        sys.stdout.write('*** ' + current_file + ':' + line_number + '\n')
-    if not filename_m:
-        sys.stdout.write(line)
+    if format:
+        hunk_m = re.match(HUNK_HEADER_PATTERNS[format], line)
+        if hunk_m:
+            line_number = hunk_m.group(1)
+            # ZK Diff Navigation annotation
+            sys.stdout.write('*** ' + current_file + ':' + line_number + '\n')
+    sys.stdout.write(line)
