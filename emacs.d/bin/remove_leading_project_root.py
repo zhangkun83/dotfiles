36273484d83c654b9135bff2201784a933e6b8f0
Kun Zhang <arthur.kun@gmail.com>
2022-05-08 23:15:50 -0700

    emacs: remove_leading_project_root.py removes excessive leading slashes

--- emacs.d/bin/remove_leading_project_root.py
+++ emacs.d/bin/remove_leading_project_root.py
@@ -9,4 +9,10 @@ ZK_PROJECT_ROOT = os.getenv('ZK_PROJECT_ROOT')
 for line in sys.stdin:
     if line.startswith(ZK_PROJECT_ROOT):
         line = line[len(ZK_PROJECT_ROOT):]
+        # Remove leading slashes of the remaining part in case there
+        # are more than one slashes after ZK_PROJECT_ROOT due to path
+        # concatenation.  Keeping those slashes will make the result
+        # look like an absolute path which is incorrect.
+        while line.startswith("/"):
+            line = line[1:]
     sys.stdout.write(line)
