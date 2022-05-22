0d45acd7e467bb5a6101e4992e93bfee8a1f9442
Kun Zhang <arthur.kun@gmail.com>
2022-05-21 21:48:17 -0700

    emacs: zk-diff-navigate uses locus to the first diff line instead of the first line of the hunk.
    git diff etc put a few common lines at the beginnging of a hunk. Although that provides
    useful context in the diff output, it's more intuitive to locate the locus at the first
    diff line.

--- emacs.d/bin/zk-transform-patch.py
+++ emacs.d/bin/zk-transform-patch.py
@@ -17,8 +17,21 @@ HUNK_HEADER_PATTERNS = {
     "git": re.compile(u'^@@ -[0-9,]+ \+([0-9]+),[0-9]+ @@'),
 }
 
+COMMON_LINE_PATTERNS = {
+    "diff-u-p4": re.compile(u'^ '),
+    "git": re.compile(u'^ '),
+}
+
+def write(line):
+    sys.stdout.write(line)
+
 format = None
 current_file = None
+# Has value if we are only seeing the first common lines of the
+# current hunk, and still waiting for the first diff line of the
+# current hunk to appear.
+hunk_start_line_number = None
+hunk_buffered_common_lines = []
 for line in sys.stdin:
     filename_m = None
     if not format:
@@ -36,9 +49,31 @@ for line in sys.stdin:
     if format:
         hunk_m = re.match(HUNK_HEADER_PATTERNS[format], line)
         if hunk_m:
-            line_number = hunk_m.group(1)
-            # ZK Diff Navigation annotation
-            sys.stdout.write('=' * 80 + '\n')
-            sys.stdout.write('*** ' + current_file + ':' + line_number + '\n')
-            sys.stdout.write('=' * 80 + '\n')
-    sys.stdout.write(line)
+            hunk_start_line_number = int(hunk_m.group(1))
+        elif hunk_start_line_number:
+            # The first few lines of a hunk are common
+            # lines. hunk_start_line_number != None means we are still
+            # processing those common lines.  We will buffer those
+            # common lines and wait until the first diff line to
+            # appear and write its line number in the navigation
+            # annotation.
+            common_line_m = re.match(COMMON_LINE_PATTERNS[format], line)
+            if common_line_m:
+                hunk_buffered_common_lines.append(line)
+            else:
+                # ZK Diff Navigation annotation
+                write('=' * 80 + '\n')
+                write('*** %s:%d\n' %
+                      (current_file,
+                       (hunk_start_line_number + len(hunk_buffered_common_lines))))
+                write('=' * 80 + '\n')
+                # Write the buffered common lines
+                for buffered_line in hunk_buffered_common_lines:
+                    write(buffered_line)
+                # Subsequent lines of this hunk will not be buffered
+                hunk_start_line_number = None
+                hunk_buffered_common_lines = []
+    write(line)
+
+assert not hunk_start_line_number
+assert not hunk_buffered_common_lines
