7b38190790409c95f9359e065f386cc3c65aba7a
Kun Zhang <zhangkun@google.com>
2022-08-03 14:33:02 -0700

    openbox: make wallpaper-auto-switcher send a notification every 30 minutes

--- config#openbox/scripts/wallpaper-auto-switcher.sh
+++ config#openbox/scripts/wallpaper-auto-switcher.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 # Switch wallpaper at every new hour
+# Also send a notification on every 30-minute mark.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sleep 10
 LAST_SWITCH_HOUR="$(date +%Y%m%d%H)"
+LAST_NOTIFY_MINUTE=""
 "$DIR/prepare-wallpaper.sh"
 while :
 do
+    CURRENT_MINUTE="$(date +%Y%m%d%H%M)"
+    if [[ "$LAST_NOTIFY_MINUTE" != "$CURRENT_MINUTE" && "$CURRENT_MINUTE" =~ .*(30|00)$ ]]; then
+        $DIR/zknotify.sh "The time is $(date +"%I:%M %p")."
+        LAST_NOTIFY_MINUTE="$CURRENT_MINUTE"
+    fi
     CURRENT_HOUR="$(date +%Y%m%d%H)"
     if [[ "$LAST_SWITCH_HOUR" != "$CURRENT_HOUR" ]]; then
         "$DIR/prepare-wallpaper.sh"
         LAST_SWITCH_HOUR="$CURRENT_HOUR"
     fi
-    sleep 60
+    # Make sure we check the time at least once per minute so that can
+    # see the 30-minute mark.
+    sleep 45
 done
