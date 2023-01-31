b41e61a46ba8673906e04871f8e3887983114268
Kun Zhang <zhangkun@google.com>
2023-01-31 09:51:46 -0800

    openbox: use DejaVu Sans Mono for urxvt

--- config#openbox/scripts/urxvt.sh
+++ config#openbox/scripts/urxvt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-urxvtc --inputMethod ibus --font "xft:Liberation Mono:size=11" --letterSpace -1 -sr -bc -st\
+urxvtc --inputMethod ibus --font "xft:DejaVu Sans Mono:size=11" --letterSpace -1 -sr -bc -st\
       -icon "${DIR}/terminal.svg" "$@"
