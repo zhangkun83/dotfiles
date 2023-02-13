23da0ae6b9f317489c94d918a94838533cfcec00
Kun Zhang <arthur.kun@gmail.com>
2023-02-13 10:48:18 -0800

    emacs: DesktopHelperServer: add instructions for using the proxy

--- emacs.d/desktop-helper/src/zk/desktophelper/DesktopHelperServer.java
+++ emacs.d/desktop-helper/src/zk/desktophelper/DesktopHelperServer.java
@@ -8,6 +8,16 @@ public class DesktopHelperServer {
   public static void main(String[] args) {
     Flags flags = new Flags(args);
     logger.info("DesktopHelperServer started with flags: " + flags);
+    System.err.println(
+        "##################################################################################\n"
+        + "Local clients will work. To make remote clients work, use:\n"
+        + "$ ssh <host> -R localhost:5035:localhost:5032 "
+        + "-t ~/.emacs.d/bin/desktop-helper-proxy\n"
+        + "to create an SSH tunnel and start a proxy.\n"
+        + "The proxy will listen on the same port (5032) as the server, while SSH will tunnel\n"
+        + "the server port 5032 on the local machine to port 5035 on the remote machine that the\n"
+        + "proxy will connect to.\n"
+        + "##################################################################################");
     int port = flags.getInt("port");
     new BlockingServer(port, new DesktopHelperServerWorker()).start();
   }
