2d912672ecb03769a7adc9ca048cb9400771fe9e
Kun Zhang <arthur.kun@gmail.com>
2023-05-02 18:49:13 -0700

    zk-org: always confirm when killing emacs

--- emacs.d/lisp/zk-org.el
+++ emacs.d/lisp/zk-org.el
@@ -153,4 +153,5 @@ the current file for completion."
 (add-hook 'org-mode-hook 'zk-org-setup-bindings)
 (add-hook 'org-agenda-mode-hook 'zk-org-setup-bindings)
 
+(setq confirm-kill-emacs 'yes-or-no-p)
 (provide 'zk-org)
