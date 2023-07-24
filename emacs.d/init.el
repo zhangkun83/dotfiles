c664261a1e02941189aebf4f085a478105410031
Kun Zhang <arthur.kun@gmail.com>
2023-07-24 16:26:45 -0700

    emacs: add browse-kill-ring.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -160,6 +160,8 @@
 (global-set-key (kbd "C-z h") 'global-hl-line-mode)
 (global-set-key (kbd "C-z j") 'zk-remove-line-breaks-within-paragraphs)
 (global-set-key (kbd "C-z c") 'zk-copy-region-to-temp-buffer)
+(require 'browse-kill-ring)
+(global-set-key (kbd "C-z k") 'browse-kill-ring)
 (global-set-key (kbd "C-M-y") 'zk-yank-to-register)
 
 (add-hook 'shell-mode-hook
