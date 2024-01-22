6b19c66ddc311ac1d793d4fd9eb458c9446d1266
Kun Zhang <arthur.kun@gmail.com>
2024-01-22 13:31:18 -0800

    zorg: use IPv4 for tunneling

--- emacs.d/bin/orglife.el
+++ emacs.d/bin/orglife.el
@@ -1,6 +1,6 @@
 (require 'zorg)
 (setq zk-zorg-rsync-backup-dir
-      "rsync://localhost:1874/backup/orglife")
+      "rsync://127.0.0.1:1874/backup/orglife")
 (setq zk-zorg-profile-name "orglife")
 (setq zk-zorg-startup-view-func
       (lambda ()
--- emacs.d/bin/orgwork.el
+++ emacs.d/bin/orgwork.el
@@ -1,6 +1,6 @@
 (require 'zorg)
 (setq zk-zorg-rsync-backup-dir
-      "rsync://localhost:1873/x20/users/zh/zhangkun/orgwork")
+      "rsync://127.0.0.1:1873/x20/users/zh/zhangkun/orgwork")
 (setq zk-zorg-profile-name "orgwork")
 (setq zk-zorg-startup-view-func
       (lambda ()
