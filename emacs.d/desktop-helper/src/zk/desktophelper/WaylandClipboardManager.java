d79ffb13690902fd49732645bf67701f8f9faa10
Kun Zhang <zhangkun@google.com>
2025-07-07 10:39:57 -0700

    desktop-helper: remove spurious newline from wl-paste output

--- emacs.d/desktop-helper/src/zk/desktophelper/WaylandClipboardManager.java
+++ emacs.d/desktop-helper/src/zk/desktophelper/WaylandClipboardManager.java
@@ -10,6 +10,11 @@ final class WaylandClipboardManager implements ClipboardManager {
 
   @Override
   public String getClipboard() throws Exception {
-    return Utils.executeCommand(Arrays.asList(new String[] {"wl-paste"}), null);
+    String content = Utils.executeCommand(Arrays.asList(new String[] {"wl-paste"}), null);
+    // wl-paste may append a spurious newline.  Remove it
+    if (content.endsWith("\n")) {
+      content = content.substring(0, content.length() - 1);
+    }
+    return content;
   }
 }
