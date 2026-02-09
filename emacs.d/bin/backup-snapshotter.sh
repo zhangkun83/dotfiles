9e2e95fe8fcf0400fd9f3b3642660ef8d4de4045
Kun Zhang <arthur.kun@gmail.com>
2026-02-09 10:44:06 -0800

    emacs: add integrity check in backup-snapshotter.sh to detect race conditions

--- emacs.d/bin/backup-snapshotter.sh
+++ emacs.d/bin/backup-snapshotter.sh
@@ -2,16 +2,34 @@
 
 # Checks the backup directory hourly and create a snapshot zip file
 # daily.
-BACKUP_DIRNAME=backup
-SNAPSHOT_DIR=~/backup-snapshots
+BACKUP_DIRNAME="backup"
+BACKUP_DIR="$HOME/$BACKUP_DIRNAME"
+SNAPSHOT_DIR="$HOME/backup-snapshots"
+TMP_COMPARE_DIR="$HOME/tmp/backup-snapshot-to-compare"
 SNAPSHOTS_LIMIT=90
+
 mkdir -p $SNAPSHOT_DIR
 while true; do
     SNAPSHOT_FILE="$SNAPSHOT_DIR/$BACKUP_DIRNAME-$(date +%Y%m%d).zip"
     if [[ ! -f "$SNAPSHOT_FILE" ]]; then
-	echo "$(date): Making snapshot $SNAPSHOT_FILE"
-	cd
-	zip -r "$SNAPSHOT_FILE" "$BACKUP_DIRNAME" && echo "Done"
+        while true; do
+	    echo "$(date): Making snapshot $SNAPSHOT_FILE"
+	    cd
+	    zip -r "$SNAPSHOT_FILE" "$BACKUP_DIRNAME" && echo "Done.  Waiting for integrity check."
+            # Wait for a minute, unzip the archive and check for
+            # integrity.  This is to detect race condition where the
+            # files are being written at the same time the snapshot is
+            # made.
+            sleep 60
+            rm -rf "$TMP_COMPARE_DIR"
+            mkdir -p "$TMP_COMPARE_DIR"
+            cd "$TMP_COMPARE_DIR" &&\
+                unzip "$SNAPSHOT_FILE" && cd "$BACKUP_DIRNAME" && diff -rq . "$BACKUP_DIR" &&\
+                echo "Integrity check successful.  Waiting for next scheduled snapshot." &&\
+                break
+            echo "Integrity check failed.  Will retry."
+            rm "$SNAPSHOT_FILE"
+        done
         # Delete old files if number has reached limit
 	cd "$SNAPSHOT_DIR"
 	while [[ $(ls -1 | wc -l) -gt $SNAPSHOTS_LIMIT ]]; do
