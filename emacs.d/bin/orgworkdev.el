e6b76e88284bd74b2ac10eed5bc092141cf9ddd9
Kun Zhang <arthur.kun@gmail.com>
2026-07-24 09:10:28 -0700

    emacs/zorg: add orgworkdev profile

---
+++ emacs.d/bin/orgworkdev.el
@@ -0,0 +1,11 @@
+(require 'zorg)
+(setq zk-zorg-rsync-backup-dir
+      "rsync://127.0.0.1:1873/repo/orgworkdev")
+(setq zk-zorg-profile-name "orgworkdev")
+(setq zk-zorg-meeting-notes-level 2)
+(setq zk-zorg-startup-view-func
+      (lambda ()
+        (org-agenda-list)
+        (zk-zorg-open-tbs-agenda)))
+
+(zk-zorg-startup-init)
